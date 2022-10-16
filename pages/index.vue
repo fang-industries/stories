3670e18d4911e37a49b50f97a81fda1515533ae1
jason <pnxl@pm.me>
2022-10-16 15:25:25 +0700

    remove nuxt link from placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -18,22 +18,9 @@
         </div>
       </nuxt-link>
     </div>
-    <div class="w-[32rem] bg-[rgb(240,240,240)] dark:bg-neutral-900 px-4 py-8">
-      <nuxt-link
-        v-for="episode in episodes"
-        :to="'/' + episode.slug"
-        class="w-fit no-underline"
-      >
-        <div class="flex flex-col">
-          <p class="text-sm no-underline dark:text-neutral-500">
-            Episode #{{ episode.num }}
-          </p>
-          <span class="underline font-medium">
-            {{ episode.title }}
-          </span>
-        </div>
-      </nuxt-link>
-    </div>
+    <div
+      class="w-[32rem] bg-[rgb(240,240,240)] dark:bg-neutral-900 px-4 py-8"
+    ></div>
     <div
       class="w-full bg-neutral-100 flex justify-center text-neutral-800 dark:bg-[rgb(27,27,27)] dark:text-neutral-200"
     >
