22be7884b322d0f3c463921e8377cf7ce1574864
jason <pnxl@pm.me>
2022-10-16 14:15:45 +0700

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="min-h-screen min-w-full bg-neutral-100 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200"
+  >
+    <div class="w-[48rem]">
+      <Nuxt />
+    </div>
+  </div>
+</template>
