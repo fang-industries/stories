5b94ee3d6f071a364044b53933f6750707bd3e91
jason <pnxl@pm.me>
2022-10-18 19:26:19 +0700

    add top margin

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -111,7 +111,7 @@
               class="prose dark:prose-invert prose-neutral max-w-none prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-2xl prose-h4:text-xl dark:prose-p:text-neutral-300 prose-p:text-neutral-700 prose-a:text-lime-500 dark:prose-a:text-lime-400 prose-a:underline prose-a:transition hover:prose-a:text-lime-700 dark:hover:prose-a:text-lime-200 prose-blockquote:font-normal prose-code:rounded-md dark:prose-code:bg-neutral-900 prose-code:bg-neutral-200 prose-code:bg-opacity-60 dark:prose-code:bg-opacity-60 prose-code:p-1 prose-code:font-mono prose-p:font-normal prose-code:text-green-600 dark:prose-code:text-green-300 prose-li:marker:text-neutral-500 prose-code:before:content-none prose-code:after:content-none prose-img:rounded-xl"
             />
           </article>
-          <footer class="-16 flex w-full flex-col gap-8 mb-8 md:mb-0">
+          <footer class="mt-16 flex w-full flex-col gap-8 mb-8 md:mb-0">
             <hr class="border-t-2 border-dashed border-neutral-600" />
             <div class="flex justify-between">
               <span class="text-neutral-600 dark:text-neutral-500"
