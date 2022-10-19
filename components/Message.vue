043ac56ae853a4c8f153ca20eccdb818c1bcd8a5
jason <pnxl@pm.me>
2022-10-19 10:40:16 +0700

    add chat messages

---
+++ components/Message.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    :class="
+      (alignment === 'right' ? 'justify-end' : 'justify-start') +
+      ' w-[48rem] flex'
+    "
+  >
+    <div class="flex flex-col">
+      <span
+        :class="
+          (alignment === 'right' ? 'pr-12 pl-2 text-right' : 'pl-12 pr-2') +
+          '  text-neutral-500 text-sm pb-2 bg-gradient-to-br'
+        "
+      >
+        {{ contact }}
+      </span>
+      <div class="flex gap-x-3">
+        <img
+          :class="
+            (alignment === 'right' ? 'hidden' : null) +
+            ' w-8 h-8 rounded-full my-auto'
+          "
+          :src="img"
+        />
+        <p
+          :class="
+            (alignment === 'right'
+              ? 'from-lime-500 to-lime-600'
+              : 'dark:from-neutral-600 dark:to-neutral-700') +
+            ' w-fit py-2 px-4 rounded-xl text-neutral-100 bg-gradient-to-br my-auto'
+          "
+        >
+          <slot />
+        </p>
+        <img
+          :class="
+            (alignment === 'left' ? 'hidden' : null) +
+            ' w-8 h-8 rounded-full my-auto'
+          "
+          :src="img"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    alignment: String,
+    img: String,
+    contact: String,
+  },
+};
+</script>
---
+++ components/MessageContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="flex flex-col gap-y-8 py-8 my-8 border-y-2 dark:border-neutral-600 border-neutral-500"
+  >
+    <div class="flex flex-col gap-y-1">
+      <span
+        class="text-sm text-center text-neutral-500 dark:text-neutral-400"
+        >{{ platform }}</span
+      >
+      <span
+        class="text-sm text-center font-semibold text-neutral-600 dark:text-neutral-300"
+        >{{ time }}</span
+      >
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    platform: String,
+    time: String,
+  },
+};
+</script>
