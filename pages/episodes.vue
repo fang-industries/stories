dc350530b3cc011e0291ea3273ef5c149de4519f
jason <pnxl@pm.me>
2022-12-23 20:01:07 +0700

    move to /main-storyline

--- pages/episodes.vue
+++ pages/episodes.vue
@@ -1,23 +1,63 @@
 <template>
   <div
-    class="dark:selection:bg-lime-400 dark:selection:text-neutral-800 selection:bg-lime-600 selection:text-neutral-200"
+    class="
+      dark:selection:bg-lime-400
+      dark:selection:text-neutral-800
+      selection:bg-lime-600
+      selection:text-neutral-200
+    "
   >
     <div class="fixed z-30 bottom-0 w-screen">
       <div
-        class="bg-gradient-to-t w-full dark:from-neutral-900 dark:to-transparent from-neutral-50 to-transparent py-8"
+        class="
+          bg-gradient-to-t
+          w-full
+          dark:from-neutral-900
+          dark:to-transparent
+          from-neutral-50
+          to-transparent
+          py-8
+        "
       ></div>
       <button
         @click="$router.go(-1)"
-        class="dark:bg-neutral-900 bg-neutral-50 w-full text-center py-1 underline text-lime-500"
+        class="
+          dark:bg-neutral-900
+          bg-neutral-50
+          w-full
+          text-center
+          py-1
+          underline
+          text-lime-500
+        "
       >
         Hide Episodes
       </button>
     </div>
     <div
-      class="h-screen w-screen flex-col gap-y-4 flex dark:bg-neutral-900 bg-[rgb(240,240,240)] px-4 md:px-8 py-8 md:py-16 overflow-y-auto"
+      class="
+        h-screen
+        w-screen
+        flex-col
+        gap-y-4
+        flex
+        dark:bg-neutral-900
+        bg-[rgb(240,240,240)]
+        px-4
+        md:px-8
+        py-8
+        md:py-16
+        overflow-y-auto
+      "
     >
       <h4
-        class="text-xl font-semibold font-display dark:text-neutral-200 text-neutral-800 p-2"
+        class="
+          text-xl
+          font-semibold font-display
+          dark:text-neutral-200
+          text-neutral-800
+          p-2
+        "
       >
         Episode Index — {{ episodes.length }}
       </h4>
@@ -28,7 +68,16 @@
         class="w-full no-underline"
       >
         <div
-          class="flex flex-col p-2 rounded-lg dark:hover:bg-neutral-700 hover:bg-neutral-300 dark:hover:bg-opacity-40 hover:bg-opacity-20 transition"
+          class="
+            flex flex-col
+            p-2
+            rounded-lg
+            dark:hover:bg-neutral-700
+            hover:bg-neutral-300
+            dark:hover:bg-opacity-40
+            hover:bg-opacity-20
+            transition
+          "
         >
           <p class="text-sm no-underline text-neutral-500">
             Episode #{{ episode.num }} • {{ episode.writtenOn }}
@@ -45,7 +94,7 @@
 <script>
 export default {
   async asyncData({ $content }) {
-    const episodes = await $content()
+    const episodes = await $content("main-storyline")
       .only(["slug", "title", "num", "writtenOn"])
       .sortBy("num", "asc")
       .fetch();
